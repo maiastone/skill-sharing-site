050819074c91f19e8902e471d01f8fd95877a3bb
Maia Stone <maiadov@gmail.com>
2017-01-19 14:46:03 -0700

    WIP setting up server functions

--- server.js
+++ server.js
@@ -7,19 +7,146 @@ const fileServer = ecstatic({
 });
 const router = new Router();
 
+const talks = Object.create(null);
+const data = '';
+const waiting = [];
+const changes = [];
+
+
 http.createServer(function(request, response) {
   if (!router.resolve(request, response)) {
     fileServer(request, response);
   }
 }).listen(8000);
 
-function respond(response, status, data, type) {
+const respond = (response, status, data, type) => {
   response.writeHead(status, {
     'Content-Type': type || 'text/plain'
   });
   response.end(data);
+};
+
+const respondJason = (response, status, data) => {
+  respond(response, status, JSON.stringify(data), 'application/json');
+};
+
+const sendTalks = (talks, response) => {
+  respondJSON(response, 200 {
+    serverTime: Date.now(),
+    talks,
+  });
 }
 
-function respondJason(response, status, data) {
-  respond(response, status, JSON.stringify(data), 'application/json');
+const waitForChanges = (since, response) => {
+  let waiter = {
+    since,
+    response,
+  };
+  waiting.push(waiter);
+  setTimeout(function() {
+    let found = waiting.indexOf(waiter);
+    if (found > -1) {
+      waiting.splice(found, 1);
+      sendTalks([], response);
+    }
+  }, 90 * 1000);
+};
+
+readStreamAsJSON(stream, callback) => {
+  stream.on('data', (chunk) => {
+    data += chunk;
+  });
+  stream.on('end', () => {
+    let result, error;
+    try {
+      result = JSON.parse(data);
+    }
+    catch(e) {
+      error = e;
+    }
+    callback(error, result);
+  });
+  stream.on('error', (error) => {
+    callback(error);
+  });
 }
+
+router.add('GET', /^\/talks\/([^\/]+)$/,
+(request, response, title) => {
+  if (title in talks) {
+    respondJSON(response, 200, talks[title]);
+  } else {
+    respond(response, 404, 'No talk ' + title + ' found');
+  }
+});
+
+
+router.add('PUT', /^\/talks\/([^\/]+)$/,
+  (request, response, title) => {
+  readStreamAsJSON(request, function(error, talk) {
+    if (error) {
+      respond(response, 400, error.toString());
+    } else if (!talk ||
+        typeof talk.presenter != 'string' ||
+        typeof talk.summary != 'string') {
+      respond(response, 400, ;'Bad talk data');
+    } else {
+      talks[title] = {
+          title,
+          presenter: talk.presenter,
+          summary: talk.summary,
+          comments: []};
+      registerChange(title);
+      respond(response, 204, null);
+    }
+  });
+});
+
+router.add('POST', /^\/talks\/([^\/]+)\/comments$/,
+  (request, response, title) => {
+  readStreamAsJSON(request, (error, comment) => {
+    if (error) {
+      respond(response, 400, error.toString());
+    } else if (!comment ||
+         typeof comment.author != 'string' ||
+         typeof comment.message != 'string') {
+      respond(response, 400, 'Bad comment data');
+    } else if (title in talks) {
+      talks[title].comments.push(comment);
+      registerChange(title);
+      respond(response, 204, null);
+    } else {
+      respond(response, 404, 'No talk ' + title + ' found');
+    }
+  });
+});
+
+router.add('DELETE', /^\/talks\/([^\/]+)$/,
+(request, response, title) => {
+  if (title in talks) {
+    delete talks[title];
+    registerChange(title);
+  }
+  respond(response, 204, null);
+});
+
+router.add('GET', /^\/talks$/, (request, response) => {
+  let query = require('url').parse(request.url, true).query;
+  if (query.changesSince == null) {
+    let list = [];
+    for (let title in talks)
+      list.push(talks[title]);
+    sendTalks(list, response);
+  } else {
+    let since = Number(query.changesSince);
+    if (isNaN(since)) {
+      respond(response, 400, 'Invalid parameter');
+    } else {
+      let changed = getChangedTalks(since);
+      if (changed.length > 0)
+         sendTalks(changed, response);
+      else
+        waitForChanges(since, response);
+    }
+  }
+});
